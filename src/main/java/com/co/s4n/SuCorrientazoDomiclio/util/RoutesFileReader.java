3ba7dfc76c1f6b73eb244512f74898d81d7398c7
Teban25 <exteban.1225@gmail.com>
2020-05-13 20:48:45 -0500

    Routes reader function

--- src/main/java/com/co/s4n/SuCorrientazoDomiclio/util/RoutesFileReader.java
+++ src/main/java/com/co/s4n/SuCorrientazoDomiclio/util/RoutesFileReader.java
@@ -1,17 +1,30 @@
 package com.co.s4n.SuCorrientazoDomiclio.util;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
- * Its function is get the routes
- * to a drone
+ * Its function is get the routes to a drone
+ * 
  * @author Teban
  *
  */
 public class RoutesFileReader {
 
-	public static Optional<List<String>> getRoutesFromFile(String routesFileLocation){
-		return null;
+	public static Optional<List<String>> getRoutesFromFile(String routesFileLocation) throws IOException {
+		Optional<List<String>> routes = Optional.empty();
+		try (Stream<String> stream= Files.lines(Paths.get(routesFileLocation))) {
+			
+		} catch (IOException e) {
+			e.printStackTrace(System.out);
+			throw new IOException(
+					"There was an error trying to load the routes file, technical error: " + e.getMessage());
+		}
+		return routes;
 	}
 }
--- src/test/java/com/co/s4n/SuCorrientazoDomiclio/util/RoutesFileReaderTest.java
+++ src/test/java/com/co/s4n/SuCorrientazoDomiclio/util/RoutesFileReaderTest.java
@@ -1,5 +1,74 @@
 package com.co.s4n.SuCorrientazoDomiclio.util;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Test;
+
 public class RoutesFileReaderTest {
 
+	/**
+	 * Test that throw an IOException when a file is corrupted or there was any
+	 * problem handle the file
+	 * 
+	 * @throws IOException
+	 */
+	@Test(expected = IOException.class)
+	public void testThat_notGetAnyRoutes_whenThereIsNotFile() throws IOException {
+		// GIVEN
+		String fakeLocation = "fakeLocation.txt";
+		// WHEN
+		Optional<List<String>> emptyRoutes = RoutesFileReader.getRoutesFromFile(fakeLocation);
+		// THEN
+		assert !emptyRoutes.isPresent();
+	}
+	
+	/**
+	 * Test that throw an IOException when a file is corrupted or there was any
+	 * problem handle the file
+	 * 
+	 * @throws IOException
+	 */
+	@Test(expected = IOException.class)
+	public void testThat_throwIOException_whenFileLocationIsNull() throws IOException {
+		// GIVEN
+		String fakeLocation = null;
+		// WHEN
+		Optional<List<String>> emptyRoutes = RoutesFileReader.getRoutesFromFile(fakeLocation);
+		// THEN
+		assert !emptyRoutes.isPresent();
+	}
+
+	/**
+	 * Test that check when a file is given correctly but is empty, the process
+	 * return an empty collection.
+	 * 
+	 * @throws IOException
+	 */
+	@Test
+	public void testThat_notGetAnyRoutes_whenFileIsEmpty() throws IOException {
+		// GIVEN
+		String fakeLocation = "t";
+		// WHEN
+		Optional<List<String>> emptyRoutes = RoutesFileReader.getRoutesFromFile(fakeLocation);
+		// THEN
+		assert emptyRoutes.get().isEmpty();
+	}
+
+	/**
+	 * Test that check when a file is given correctly, the process return some
+	 * routes.
+	 * 
+	 * @throws IOException
+	 */
+	@Test
+	public void testThat_getSomeRoutes_whenFileExists() throws IOException {
+		// GIVEN
+		String fakeLocation = "t";
+		// WHEN
+		Optional<List<String>> emptyRoutes = RoutesFileReader.getRoutesFromFile(fakeLocation);
+		// THEN
+		assert !emptyRoutes.get().isEmpty();
+	}
 }
